253b9461ab6124c8ff8cfb63e942ab894c82099c
Corey Scheideman <corscheid@gmail.com>
2018-04-30 21:40:29 -0400

    change formatting, new ascii-art header text

--- drant.go
+++ drant.go
@@ -7,15 +7,29 @@ import (
   "log"
 )
 
-func printRant(rant devgorant.RantModel) {
-  fmt.Printf("<ID:%d> (+%d) {%d comments} by %s(+%d):\n%s\n%s\n\n",
-    rant.Id, rant.Score, rant.NumComments, rant.UserUsername, rant.UserScore, rant.Text, rant.Tags)
+func printRantPreview(rant devgorant.RantModel) {
+  var text string;
+  if len(rant.Text) > 160 {
+    text = rant.Text[:155]
+    text += "[...]\n[Read more...]"
+  } else {
+    text = rant.Text
+  }
+  fmt.Printf("(+%d) <ID:%d> by %s(+%d):\n%s\n%s {%d comments}\n\n",
+    rant.Score, rant.Id, rant.UserUsername, rant.UserScore, text, rant.Tags, rant.NumComments)
 }
 
-func printComments(comments []devgorant.CommentModel) {
+func printRant(rant devgorant.RantModel, comments []devgorant.CommentModel, limit int) {
+  fmt.Printf("(+%d) <ID:%d> by %s(+%d):\n%s\n%s\n",
+    rant.Score, rant.Id, rant.UserUsername, rant.UserScore, rant.Text, rant.Tags)
+
+  fmt.Printf("\nComments[%d]\n\n", rant.NumComments)
+
   for i, c := range comments {
-    fmt.Printf("[%d] <ID:%d> (+%d) by %s(+%d):\n%s\n\n",
-      i, c.Id, c.Score, c.UserUsername, c.UserScore, c.Body)
+    if i < limit {
+      fmt.Printf("(+%d) <ID:%d> by %s(+%d):\n%s\n\n",
+        c.Score, c.Id, c.UserUsername, c.UserScore, c.Body)
+    }
   }
 }
 
@@ -29,7 +43,9 @@ func main() {
   var rantsFlag, randomFlag, weeklyFlag *bool
   var sortFlag, profileFlag, searchFlag *string
   var rantFlag, limitFlag, skipFlag *int
+  var cmdPrefix, headText string
 
+  headText = "      _            ____             _\n   __| | _____   _|  _ \\ __ _ _ __ | |_\n  / _` |/ _ \\ \\ / / |_) / _` | '_ \\| __|\n | (_| |  __/\\ V /|  _ < (_| | | | | |_\n  \\__,_|\\___| \\_/ |_| \\_\\__,_|_| |_|\\__|"
   rantsFlag = flag.Bool("r", false, "fetches rants")
   sortFlag = flag.String("m", "algo", "sorting method for -r: algo, top, or recent")
   limitFlag = flag.Int("l", 50, "number of rants to fetch for -r")
@@ -48,33 +64,31 @@ func main() {
   if *rantsFlag {
     rants, err := devrant.Rants(*sortFlag, *limitFlag, *skipFlag)
     check(err)
-    for i, r := range rants {
-      fmt.Printf("[%d] ", i)
-      printRant(r)
+    for _, r := range rants {
+      //fmt.Printf("[%d] ", i)
+      printRantPreview(r)
     }
   } else if *rantFlag != -1 {
     rant, comments, err := devrant.Rant(*rantFlag)
     check(err)
-    printRant(rant)
-    fmt.Printf("Comments[%d]\n\n", rant.NumComments)
-    printComments(comments)
+    printRant(rant, comments, *limitFlag)
   } else if *randomFlag {
     rant, err := devrant.Surprise()
     check(err)
-    printRant(rant)
+    printRant(rant, nil, *limitFlag)
   } else if *weeklyFlag {
     rants, err := devrant.WeeklyRants()
     check(err)
-    for i, r := range rants {
-      fmt.Printf("[%d] ", i)
-      printRant(r)
+    for _, r := range rants {
+      //fmt.Printf("[%d] ", i)
+      printRantPreview(r)
     }
   } else if *searchFlag != "" {
     rants, err := devrant.Search(*searchFlag)
     check(err)
-    for i, r := range rants {
-      fmt.Printf("[%d] ", i)
-      printRant(r)
+    for _, r := range rants {
+      //fmt.Printf("[%d] ", i)
+      printRantPreview(r)
     }
   } else if *profileFlag != "" {
     user, content, err := devrant.Profile(*profileFlag)
@@ -82,13 +96,14 @@ func main() {
     fmt.Printf("%s(+%d)\nLocation: %s\nJoined: %d\nAbout: %s\nSkills: %s\nGitHub: %s\n",
       user.Username, user.Score, user.Location, user.CreatedTime, user.Skills, user.Github)
     fmt.Printf("Rants:\n")
-    for i, r := range content.Rants {
-      if i < *limitFlag {
-        fmt.Printf("[%d] ", i)
-        printRant(r)
-      }
+    for _, r := range content.Rants {
+        //fmt.Printf("[%d] ", i)
+        printRantPreview(r)
     }
   } else {
-    fmt.Println("devRant CLI | run drant -h for help")
+    fmt.Println(headText)
+
+    //TODO REPL
+    fmt.Print(cmdPrefix)
   }
 }
